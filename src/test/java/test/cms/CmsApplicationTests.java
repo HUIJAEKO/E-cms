c7f2ad0890fee7d7be78f971de181c41d083f976
HUIJAEKO <xrhgmlwox@naver.com>
2025-02-16 12:34:33 +0900

    first commit

---
+++ src/test/java/test/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package test.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
